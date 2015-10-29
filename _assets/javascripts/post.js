fe0fd00ae5d9187e3f1dc3a8878da1eaf7ab8a2d
Chris Wallwork <chris@unrefgugees.org>
2015-10-29 09:48:26 -0400

    friends form working

--- _assets/javascripts/post.js
+++ _assets/javascripts/post.js
@@ -48,7 +48,7 @@ function submitListener(){
 
   $form.submit(function(event){
     event.preventDefault();
-    $('.submit-button').val('please wait...').prop('disabled',true);
+    $('.submit-friends').val('please wait...').prop('disabled',true);
     $.ajax({
       type: 'POST',
       url: '//nyc.us11.list-manage.com/subscribe/post-json?u=7aa897cfc40f7cfbb83ffadd4&amp;id=710836cd94&c=?',
@@ -64,16 +64,16 @@ function submitListener(){
         if (data.result !== "success") {
           console.log(data);
           $('.submit-friends').val('Please Wait...').prop('disabled',true);
-          $('#conf-friends').html('').slideUp(700);
-          $('#conf-friends').html("Something went wrong, please try to submit your details again.").slideDown(700, function(){
+          $('.conf-friends').html('').slideUp(700);
+          $('.conf-friends').html("Something went wrong, please try to submit your details again.").slideDown(700, function(){
             $('.submit-friends').val('Raise Your Hand!').prop('disabled',false);
           });
         }
 
         else {
           console.log(data);
-            $('.signup').children().hide(400,function()
-              {$('#conf-friends').html("Thanks! Please confirm your email address.").slideDown(700);
+            $('.friends').children().hide(400,function()
+              {$('.conf-friends').html("Thanks! Please confirm your email address.").slideDown(700);
           });
         }
       }
